3feff493011930cb82b9c93bfe2f94c203d59b6b
Joel Summerfield <j.summerfield@elifesciences.org>
2019-09-30 15:10:12 +0100

    Extended library with some additional methods.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,18 @@
-import { Add } from './index';
+import { Add, Subtract, Multiply, Divide } from './index';
 
 test('Add', () => {
     expect(Add(1, 1)).toBe(2);
+});
+
+test('Subtract', () => {
+    expect(Subtract(5, 2)).toBe(3);
+});
+
+test('Multiply', () => {
+    expect(Multiply(5, 2)).toBe(10);
+});
+
+test('Divide', () => {
+    expect(Divide(10, 2)).toBe(5);
+    expect(Divide(10, 0)).toBe(10);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 export const Add = (a: number, b: number) => {
-  return a + b;
+    return a + b;
+};
+
+export const Subtract = (a : number, b : number) => {
+    return a - b;
+};
+
+export const Multiply = (a : number, b : number) => {
+    return a * b;
+};
+
+export const Divide = (a : number, b : number) => {
+    if (b === 0) b = 1;
+    return a / b;
 };
