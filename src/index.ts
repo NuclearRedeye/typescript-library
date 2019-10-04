0854ba4d9892cb91d106e172fd42b0558b304dac
Joel Summerfield <j.summerfield@elifesciences.org>
2019-10-04 16:03:46 +0100

    Fixed linting issues.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-export const Add = (a: number, b: number) : number => {
+export const Add = (a: number, b: number): number => {
     return a + b;
 };
 
-export const Subtract = (a : number, b : number) : number => {
+export const Subtract = (a: number, b: number): number => {
     return a - b;
 };
 
-export const Multiply = (a : number, b : number) : number => {
+export const Multiply = (a: number, b: number): number => {
     return a * b;
 };
 
-export const Divide = (a : number, b : number) : number => {
-    if (b === 0)
-    {
+export const Divide = (a: number, b: number): number => {
+    if (b === 0) {
         b = 1;
     }
     return a / b;
