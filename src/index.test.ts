da06a90849e8893ca3d632e15b7201d9227061f9
Joel Summerfield <52923935+joelsummerfield@users.noreply.github.com>
2020-01-28 16:53:27 +0000

    feat: automatically fix code style and formatting issues
    Added a GitHub action to automatically fix style and formatting issues in PRs.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,18 @@
 import { Add, Divide, Multiply, Subtract } from './index';
 
 test('Add', () => {
-    expect(Add(1, 1)).toBe(2);
+  expect(Add(1, 1)).toBe(2);
 });
 
 test('Subtract', () => {
-    expect(Subtract(5, 2)).toBe(3);
+  expect(Subtract(5, 2)).toBe(3);
 });
 
 test('Multiply', () => {
-    expect(Multiply(5, 2)).toBe(10);
+  expect(Multiply(5, 2)).toBe(10);
 });
 
 test('Divide', () => {
-    expect(Divide(10, 2)).toBe(5);
-    expect(Divide(10, 0)).toBe(10);
+  expect(Divide(10, 2)).toBe(5);
+  expect(Divide(10, 0)).toBe(10);
 });
