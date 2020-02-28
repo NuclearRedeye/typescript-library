09d8f799c30eb5e83a2ad56fd02113154c76b9af
NuclearRedeye <52923935+NuclearRedeye@users.noreply.github.com>
2020-02-28 10:52:28 +0000

    chore: generate a minified version

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import dts from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
 module.exports = [
   {
     input: 'build/index.js',
@@ -6,6 +7,14 @@ module.exports = [
       file: 'dist/lib/typescript-library.js',
       format: 'es',
     },
+  },
+  {
+    input: 'build/index.js',
+    output: {
+      file: 'dist/lib/typescript-library.min.js',
+      format: 'es',
+    },
+    plugins: [terser()],
   },
   {
     input: './build/index.d.ts',
