a78db5706d460570aade3a9f32af9097e104129c
NuclearRedeye <52923935+NuclearRedeye@users.noreply.github.com>
2020-02-28 16:33:49 +0000

    chore: added some initial samples

---
+++ samples/node/index.js
@@ -0,0 +1,6 @@
+import { Add, Subtract, Multiply, Divide } from '../../dist/lib/typescript-library.js';
+
+console.log(`2 + 2 = ${Add(2, 2)}`);
+console.log(`2 - 2 = ${Subtract(2, 2)}`);
+console.log(`2 * 2 = ${Multiply(2, 2)}`);
+console.log(`2 / 2 = ${Divide(2, 2)}`);
