b5d9628c28b81e56d3f4301f43330bc697a0c828
Joel Summerfield <52923935+NuclearRedeye@users.noreply.github.com>
2021-04-15 22:40:18 +0100

    feat: update to use a docker based build system (#23)

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,10 @@
 import dts from 'rollup-plugin-dts';
-import { terser } from 'rollup-plugin-terser';
+
 module.exports = [
   {
-    input: 'build/index.js',
+    input: './out/debug/index.d.ts',
     output: {
-      file: 'dist/lib/typescript-library.js',
-      format: 'es',
-    },
-  },
-  {
-    input: 'build/index.js',
-    output: {
-      file: 'dist/lib/typescript-library.min.js',
-      format: 'es',
-    },
-    plugins: [terser()],
-  },
-  {
-    input: './build/index.d.ts',
-    output: {
-      file: 'dist/lib/typescript-library.d.ts',
+      file: './out/release/index.d.ts',
       format: 'es',
     },
     plugins: [dts()],
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { Add, Divide, Multiply, Subtract } from '../src/index';
+import { Add, Divide, Multiply, Subtract } from './index';
 
 test('Add', () => {
   expect(Add(1, 1)).toBe(2);
